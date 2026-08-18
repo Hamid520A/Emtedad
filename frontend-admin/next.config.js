63efa4d7ea579731c39d2743b2fccc3621f427e8
hamid <hamid24621937@gmail.com>
2026-08-18 09:50:18 +0330

    admin panel login fixed

--- frontend-admin/next.config.js
+++ frontend-admin/next.config.js
@@ -5,15 +5,20 @@ const nextConfig = {
     workerThreads: false, 
   },
   async rewrites() {
-    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://backend:8000';
+    // 🌟 آدرس واقعی بک‌اند را مستقیماً اینجا وارد می‌کنیم
+    // از آنجایی که بک‌اند و فرانت‌ند در یک سرور هستند، مستقیماً به آی‌پی و پورت بک‌اند پاس می‌دهیم
+    const backendUrl = 'http://10.10.20.51:64000'; 
+    
+    // (اگر فرانت‌ند و بک‌اند در یک فایل docker-compose.yml هستند، حتی می‌توانید از 'http://backend:8000' هم استفاده کنید)
+    
     return [
       {
         source: '/api/:path*',
-        destination: `${backendUrl}/:path*`,
+        destination: `${backendUrl}/:path*`, // ترافیک به صورت مخفیانه به بک‌اند می‌رود
       },
       {
         source: '/static/:path*',
-        destination: `${backendUrl}/static/:path*`,
+        destination: `${backendUrl}/static/:path*`, // برای لود شدن عکس‌ها و جزوه‌ها
       },
     ];
   },
